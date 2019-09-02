bb0a65012938022089d61cb651789cb4e489336f
Michael Odumosu <michaelodumosu57@gmail.com>
2019-09-02 06:17:20 -0400

    learned about Jasmine spies, and more about how to setup app environs for your test

--- spec/support/spec/jasmine_examples/PlayerSpec.js
+++ spec/support/spec/jasmine_examples/PlayerSpec.js
@@ -57,4 +57,108 @@ describe("Player", function() {
       }).toThrowError("song is already playing");
     });
   });
+  
+  
+    xdescribe("skips this test",()=>{
+        beforeEach(function() {
+            foo = 0;
+            foo += 1;
+        });
+        
+        it("is just a function, so it can contain any code", function() {
+            expect(foo).toEqual(1);
+        });
+    })
+    
+    describe("Pending specs", function() {
+        xit("can be declared 'xit'", function() {
+            expect(true).toBe(false);
+        });
+        
+        it("can be declared with 'it' but without a function");
+        
+        it("can be declared by calling 'pending' in the spec body", function() {
+            expect(true).toBe(false);
+            pending('this is why it is pending');
+        });
+    });
+    
+    
+describe("A spy", function() {
+  var foo, bar = null;
+
+  beforeEach(function() {
+    foo = {
+      setBar: function(value) {
+        bar = value;
+      }
+    };
+    
+    spyOn(foo, 'setBar');
+
+    foo.setBar(123);
+    foo.setBar(456, 'another param');
+  });
+  
+  
+it("tracks that the spy was called", function() {
+    expect(foo.setBar).toHaveBeenCalled();
+  });
+  
+it("tracks that the spy was called x times", function() {
+    expect(foo.setBar).toHaveBeenCalledTimes(2);
+  });
+  
+it("tracks all the arguments of its calls", function() {
+    expect(foo.setBar).toHaveBeenCalledWith(123);
+    expect(foo.setBar).toHaveBeenCalledWith(456, 'another param');
+  });
+
+it("stops all execution on a function", function() {
+    expect(bar).toBeNull();
+  });
+  
+it("tracks if it was called at all", function() {
+    foo.setBar();
+
+    expect(foo.setBar.calls.any()).toEqual(true);
+  });
+});
+  
+describe("A spy, when created manually", function() {
+  var whatAmI;
+
+  beforeEach(function() {
+    whatAmI = jasmine.createSpy('whatAmI');
+
+    whatAmI("I", "am", "a", "spy");
+  });
+
+  it("tracks that the spy was called", function() {
+    expect(whatAmI).toHaveBeenCalled();
+  });
+});
+
+
+describe("Multiple spies, when created manually", function() {
+  var tape;
+
+  beforeEach(function() {
+    tape = jasmine.createSpyObj('tape', ['play', 'pause', 'stop', 'rewind']);
+
+    tape.play();
+    tape.pause();
+    tape.rewind(0);
+  });
+
+  it("creates spies for each requested function", function() {
+    expect(tape.play).toBeDefined();
+    expect(tape.pause).toBeDefined();
+    expect(tape.stop).toBeDefined();
+    expect(tape.rewind).toBeDefined();
+  });
+});
+
+
+          
 });
